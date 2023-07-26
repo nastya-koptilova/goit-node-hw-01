df2002da7f75fa621fe8246b458f116b33249844
Anastasia Koptilova <koptilova.nastya@gmail.com>
2023-07-26 20:26:13 +0300

    add files

---
+++ index.js
@@ -0,0 +1,43 @@
+const contacts = require("./db/contacts");
+const { Command } = require('commander');
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const list = await contacts.listContacts();
+      console.log(list);
+      break;
+
+    case "get":
+      const contactById = await contacts.getContactById(id);
+      console.log(contactById);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const remivedContact = await contacts.removeContact(id);
+      console.log(remivedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+      break;
+  }
+};
+
+invokeAction(argv);
